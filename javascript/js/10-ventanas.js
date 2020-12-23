fe8de56a8205e2f3b94b4ced4fa74cf7418ec57a
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-23 17:54:27 -0500

    Alertas y ventanas de ingreso de datos

---
+++ javascript/js/10-ventanas.js
@@ -0,0 +1,13 @@
+'user strict'
+
+//ALERTA
+// alert("Esta es mi alerta!!");
+// alert("Esta es mi texto!!");
+
+//CONFIRMACIÓN
+// var mi_resultado = confirm("¿Estas seguro de querer continuar?");
+// console.log(mi_resultado);
+
+//INGRESO DATOS
+var mi_promt = prompt("¿Que edad tienes?", 18);
+console.log(mi_promt);
