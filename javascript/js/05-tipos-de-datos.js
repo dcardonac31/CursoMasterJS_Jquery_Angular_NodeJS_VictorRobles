48438e1740d9603ad22160a52cfd9394d9659be0
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-23 10:50:11 -0500

    Operadores y tipos de datos

---
+++ javascript/js/05-tipos-de-datos.js
@@ -0,0 +1,21 @@
+'use strict'
+
+//Operadores
+//+ - * / %
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+alert("Resultado: "+ operacion);
+
+//Tipos de datos
+var numero_entero = 44;
+var cadena_texto = "Hola que tal";
+var verdadero_o_falso = true;
+
+var numero_falso = "33";
+
+console.log(parseInt(numero_falso)+7);
+console.log(parseFloat(numero_falso)+7);
+console.log(numero_entero+'texto concatenado')
+console.log(String(numero_entero)+4);
