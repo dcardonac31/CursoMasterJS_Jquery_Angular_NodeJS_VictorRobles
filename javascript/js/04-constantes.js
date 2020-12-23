4c88fef9859041a888d9e893d8f6553d46a7ac73
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-23 10:36:06 -0500

    Constantes

---
+++ javascript/js/04-constantes.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//constantes
+// como una variable, pero su valor no puede cambiar
+
+var web = 'https://victorroblesweb.es';
+const ip = '192.88.0.12';
+
+web = "https://victorroblescursos.es";
+ip = "192.88.0.13";
+console.log(web, ip);
+
+//Sale error: 
+    //04-constantes.js:10 Uncaught TypeError: Assignment to constant variable.
+    //at 04-constantes.js:10
+    //(anonymous) @ 04-constantes.js:10
