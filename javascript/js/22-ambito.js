178a1c425dc5eb908115afde9557140114e7d3ba
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-26 18:40:15 -0500

    El ámbito de las variables

---
+++ javascript/js/22-ambito.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+
+function holaMundo(texto){
+  var hola_mundo = "Texto dentro de función";
+  console.log(texto);
+  console.log(numero.toString());
+  console.log(hola_mundo);
+  console.log(numero2);
+}
+
+var numero = 12;
+var texto = "Hola mundo soy una variable global";
+holaMundo(texto);
+let numero2 = 13;
+
