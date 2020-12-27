788f0e3b6b9a76d93cd69970a9c69ec735a3d08a
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-27 00:10:52 -0500

    Funciones de reemplazo

--- javascript/js/23-funciones-texto.js
+++ javascript/js/23-funciones-texto.js
@@ -5,6 +5,7 @@
 var numero = 444;
 var texto1 = "Bienvenido al curso de JavaScript curso de Victor Robles";
 var texto2 = "es muy buen curso";
+var texto3 = " Bienvenido al curso de JavaScript curso de Victor Robles ";
 
 // var busqueda = texto1.indexOf("curso");
 // var busqueda = texto1.search("curso");
@@ -14,5 +15,9 @@ var texto2 = "es muy buen curso";
 // var busqueda = texto1.charAt(14); //extraer posicion en concreto
 // var busqueda = texto1.startsWith("Bi"); //Buscar si el texto comienza con caracter o texto especifico
 // var busqueda = texto1.endsWith("es"); //Buscar si el texto comienza con caracter o texto especifico
-var busqueda = texto1.includes("JavasCript"); //Buscar texto exacto
+// var busqueda = texto1.includes("JavasCript"); //Buscar texto exacto
+// var busqueda = texto1.replace("JavaScript", "Symfony"); //Reemplazar texto
+// var busqueda = texto1.slice(14,22); //cortar texto en una posicion
+// var busqueda = texto1.split(" "); //separar el texto por determinado caracter y llevarlo a un array
+var busqueda = texto3.trim(); //quita espacios al comienzo y final
 console.log(busqueda);
