57fb9278e81fcacdf4452847ff6c9dc19498ce30
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-26 19:33:15 -0500

    Métodos de búsqueda

--- javascript/js/23-funciones-texto.js
+++ javascript/js/23-funciones-texto.js
@@ -3,26 +3,16 @@
 // Transformación de textos
 
 var numero = 444;
-var texto1 = "Bienvenido al curso de JavaScript de Victor Robles";
+var texto1 = "Bienvenido al curso de JavaScript curso de Victor Robles";
 var texto2 = "es muy buen curso";
 
-var dato = numero.toString(); // numero a texto
-    dato = texto1.toUpperCase() // mayusculas
-    dato = texto2.toLowerCase() // minusculas
-
-
-
-console.log(dato);
-
-// Calcular longitud texto
-var nombre = "David";
-// console.log(nombre.length);
-nombre = ["David", "Cardona"]
-// console.log(nombre.length);
-
-//Concatenar - Unir textos
-
-//var textoTotal = texto1+" "+texto2;
-
-var textoTotal = texto1.concat(" "+texto2);
-console.log(textoTotal);
+// var busqueda = texto1.indexOf("curso");
+// var busqueda = texto1.search("curso");
+// var busqueda = texto1.match("curso");
+// var busqueda = texto1.match(/curso/g); //Buscar varios elementos
+// var busqueda = texto1.substr(14,5); //extraer caracteres de una posicion a otra
+// var busqueda = texto1.charAt(14); //extraer posicion en concreto
+// var busqueda = texto1.startsWith("Bi"); //Buscar si el texto comienza con caracter o texto especifico
+// var busqueda = texto1.endsWith("es"); //Buscar si el texto comienza con caracter o texto especifico
+var busqueda = texto1.includes("JavasCript"); //Buscar texto exacto
+console.log(busqueda);
