be78f5a5589d084015dd1a79b7d52de522852df4
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-31 00:30:34 -0500

    Arrays avanzados

--- javascript/js/25-arrays.js
+++ javascript/js/25-arrays.js
@@ -5,7 +5,25 @@
 var nombre = "David Cardona";
 var nombres = ["David Cardona", "Juan Lopez", "Manolo Garcia", 52, true]
 
-var lenguajes = new Array("PHP", "JS", "Go", "Java", "C#");
+var lenguajes = new Array("PHP", "JS", "Go", "Java", "C#", "C", "RPGLE");
 
 console.log(nombres[2]);
 console.log(lenguajes[3]);
+
+console.log(nombres.length);
+
+// var elemento = parseInt(prompt("¿Que elemento del array quieres?",0));
+// if(elemento >= nombres.length){
+//   alert("Introduce numero menor que: "+ nombres.length);
+// }else{
+//   alert("Valor elemento: "+nombres[elemento])
+// }
+
+document.write("<h1>Lenguajes de programación del 2018</h1>");
+document.write("<ul>");
+for (let i = 0; i < lenguajes.length; i++) {
+  document.write("<li>"+lenguajes[i]+"</li>");
+  document.write("</br>");
+  
+}
+document.write("</ul>");
