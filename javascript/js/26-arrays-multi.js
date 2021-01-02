6297466230f6d32377da761b29406d22694d2bae
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-01-02 11:21:50 -0500

    Arrays multidimensionales

---
+++ javascript/js/26-arrays-multi.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var categorias = ['Acción', 'Terror', 'Comedia'];
+var peliculas = ['La verdad duele', 'La vida es bella', 'Gran Torino'];
+
+var cine = [categorias, peliculas]
+
+console.log(cine[0][1]);
+console.log(cine[1][2]);
+
+
+
