7b75df71ad50322bd78f3b0ce6cf4b737b5150ac
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-01-03 18:55:23 -0500

    Operaciones con arrays

--- javascript/js/26-arrays-multi.js
+++ javascript/js/26-arrays-multi.js
@@ -1,12 +1,31 @@
 'use strict'
 
 var categorias = ['Acción', 'Terror', 'Comedia'];
-var peliculas = ['La verdad duele', 'La vida es bella', 'Gran Torino'];
+var peliculas = ['La verdad duele', 'La vida es bella', 'Gran torino'];
 
 var cine = [categorias, peliculas]
 
-console.log(cine[0][1]);
-console.log(cine[1][2]);
+// console.log(cine[0][1]);
+// console.log(cine[1][2]);
+
+// var elemento = prompt("Introduce tu pelicula: ");
+
+// do{
+//   elemento = prompt("Introduce tu pelicula:")
+//   peliculas.push(elemento);
+// }while(elemento != "ACABAR")
+
+// peliculas.pop();
+
+var indice = peliculas.indexOf("Gran torino")
+
+if(indice > -1){
+  peliculas.splice(indice, 1);
+}
+
+var peliculasString = peliculas.join();
+
+console.log(peliculasString);
 
 
 
